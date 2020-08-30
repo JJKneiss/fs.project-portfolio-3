899f56b8bad324f14be05bd3d22f1c7107d4518e
JJKneiss <54962857+JJKneiss@users.noreply.github.com>
2020-08-30 19:25:14 -0400

    Add async function, Pull Data

--- openSourceAPI/js/recipes.js
+++ openSourceAPI/js/recipes.js
@@ -5,3 +5,19 @@ var index = 1;
 const apiKey = "572f9b42c48b46fdb51c720d96b43f5b";
 var recipeIds = ["716427", "716429", "716430"];
 const startURL = "https://api.spoonacular.com/recipes/";
+
+getRecipes(index);
+
+async function getRecipes(index){
+    const endURL = recipeIds[index] + "/information?apiKey=" + apiKey;
+    var url = startURL + endURL;
+    const response = await fetch(url); 
+    const data = await response.json();
+    document.querySelector('img').width = 500;
+    document.querySelector('img').height = 500;
+    document.querySelector('img').src = data.image;
+    document.querySelector('h3').innerHTML = data.title;
+    var dd = document.querySelectorAll('dd');
+    dd[0].innerHTML  = data.servings;
+    dd[1].innerHTML = data.readyInMinutes;
+}
