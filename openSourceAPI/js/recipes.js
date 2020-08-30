dd9ba223baeb1eb62c6aae700cdad1b1b7a28db8
JJKneiss <54962857+JJKneiss@users.noreply.github.com>
2020-08-30 19:31:16 -0400

    Commenting & Finish

--- openSourceAPI/js/recipes.js
+++ openSourceAPI/js/recipes.js
@@ -1,10 +1,13 @@
 document.querySelector('h2').innerHTML = 
 "<strong>A La Carte </strong> Recipes";
 
+// API Call & Info
 var index = 1;
 const apiKey = "572f9b42c48b46fdb51c720d96b43f5b";
 var recipeIds = ["716427", "716429", "716430"];
 const startURL = "https://api.spoonacular.com/recipes/";
+
+// Prvious & Next Buttons
 const previous = document.querySelectorAll('button');
 if (previous)
 {
@@ -17,6 +20,8 @@ if (next)
     console.log(next[1]);
     next[1].addEventListener("click", nextRecipe);
 }
+
+// Get Recipes 
 getRecipes(index);
 async function getRecipes(index){
     const endURL = recipeIds[index] + "/information?apiKey=" + apiKey;
@@ -32,6 +37,7 @@ async function getRecipes(index){
     dd[1].innerHTML = data.readyInMinutes;
 }
 
+//Switch Recipes
 function previousRecipe(event){
     event.preventDefault();
     if (!(index > 0)) return
