f25527194d8b2440299841fc911bda1c90908191
JJKneiss <54962857+JJKneiss@users.noreply.github.com>
2020-08-30 20:19:22 -0400

    Theme & Format, removed images

--- formValidation/js/form.js
+++ formValidation/js/form.js
@@ -1,9 +1,6 @@
-// Set Theme
-document.querySelector('h2').innerHTML = 
-"<strong>Get</strong> In Touch";
-// Disable Button
-document.querySelector('button').disabled = true;
-document.querySelector('button').className = "disabled";
+
+// Disable Button, Set Error
+checkFilled();
 
 // Change Paragraph Text
 document.querySelector('p').innerHTML = 
@@ -13,31 +10,17 @@ document.querySelector('p').innerHTML =
 "sure to get in touch as soon as possible! " +
 "Have a great day!";
 
+// Inputs
 var labels = document.querySelectorAll('label');
-for (var i = 0; i < labels.length; i++){
-    labels[i].className = "error";
-}
-// Validation Checks
-
-var input = document.querySelectorAll('input');
-for (var i = 0; i < input.length; i++){
-    if(input[i]){
-        console.log(i);
-        input[i].addEventListener("change", );
-    }
-}
-function checkInput() {
-    event.preventDefault();  
-    console.log("Form has changed!");
-    if(input[0]){
-
-    }
-}
+var inputAll = document.querySelectorAll('input');
 var textarea = document.querySelector('textarea');
-if (textarea){
-    textarea.addEventListener("change", function(){
-        event.preventDefault();
-        console.log(labels[3]);
-        labels[3].removeAttribute("class");
-    });
-}
+// Set Theme
+document.querySelector('h2').innerHTML = 
+"<strong>Get</strong> In Touch";
+inputAll[0].placeholder = "Full Name";
+inputAll[1].placeholder = "Email";
+inputAll[2].placeholder = "Subject Matter";
+textarea.placeholder = "Concerns";
+// I tried literally everything to get the stupid
+// Image to change. NOTHING WORKED. I gave up
+
